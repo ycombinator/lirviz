5b01a0870ed814e859dff9ee5f3b1b5d02c6e0f0
Shaunak Kashyap <ycombinator@gmail.com>
2016-09-26 06:04:04 +0200

    Factory-fy-ing

--- grid.js
+++ grid.js
@@ -1,28 +1,47 @@
-function drawGrid(spacing = 10) {
-  var canvas = ctx.canvas;
-  ctx.lineWidth = 0.25;
-  ctx.strokeStyle = '#c0c0c0';
-
-  // Establish canvas sides' positions
-  var canvasLeft   = canvas.clientLeft;
-  var canvasRight  = canvasLeft + canvas.clientWidth;
-  var canvasTop    = canvas.clientTop;
-  var canvasBottom = canvasTop + canvas.clientHeight;
-
-  // Draw vertical lines in grid
-  for (position = canvasLeft + spacing; position < canvasRight; position += spacing) {
-    ctx.beginPath();
-    ctx.moveTo(position, canvasTop);
-    ctx.lineTo(position, canvasBottom);
-    ctx.stroke();
+class Grid {
+  constructor(canvas, spacing = 10) {
+    this.ctx = canvas.getContext('2d');
+    this.spacing = 10;
+    this.render();
   }
 
-  // Draw horizontal lines in grid
-  for (position = canvasTop + spacing; position < canvasBottom; position += spacing) {
-    ctx.beginPath();
-    ctx.moveTo(canvasLeft, position);
-    ctx.lineTo(canvasRight, position);
-    ctx.stroke();
+  render() {
+    var canvas = this.ctx.canvas;
+    this.ctx.lineWidth = 0.25;
+    this.ctx.strokeStyle = '#c0c0c0';
+
+    // Establish canvas sides' positions
+    var canvasLeft   = canvas.clientLeft;
+    var canvasRight  = canvasLeft + canvas.clientWidth;
+    var canvasTop    = canvas.clientTop;
+    var canvasBottom = canvasTop + canvas.clientHeight;
+
+    // Draw vertical lines in grid
+    for (var position = canvasLeft + this.spacing; position < canvasRight; position += this.spacing) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(position, canvasTop);
+      this.ctx.lineTo(position, canvasBottom);
+      this.ctx.stroke();
+    }
+
+    // Draw horizontal lines in grid
+    for (var position = canvasTop + this.spacing; position < canvasBottom; position += this.spacing) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(canvasLeft, position);
+      this.ctx.lineTo(canvasRight, position);
+      this.ctx.stroke();
+    }
   }
 
+  addPluginNode(topLeftX, topLeftY, width, height) {
+    return new PluginNode(this.ctx, topLeftX, topLeftY, width, height);
+  }
+
+  addIfNode(topLeftX, topLeftY, width, height) {
+    return new IfNode(this.ctx, topLeftX, topLeftY, width, height);
+  }
 }
+
+Grid.makeWith = (ctx, spacing) => {
+  return new Grid(ctx, spacing);
+};
--- node.js
+++ node.js
@@ -1,8 +1,9 @@
 const CURVE_RADIUS = 10;
-const IO_RADIUS = 5;
+const IO_RADIUS = 6;
 
 class Node {
-  constructor(topLeftX, topLeftY, width, height, inputs, outputs) {
+  constructor(ctx, topLeftX, topLeftY, width, height, inputs, outputs) {
+    this.ctx      = ctx;
     this.topLeftX = topLeftX;
     this.topLeftY = topLeftY;
     this.width    = width;
@@ -25,30 +26,30 @@ class Node {
     var bottomRightX = this.topLeftX + this.width;
     var bottomRightY = this.topLeftY + this.height;
 
-    ctx.beginPath();
-    ctx.lineWidth = 2.5;
-    ctx.strokeStyle = '#444';
-    ctx.fillStyle = '#ddd';
+    this.ctx.beginPath();
+    this.ctx.lineWidth = 2.5;
+    this.ctx.strokeStyle = '#444';
+    this.ctx.fillStyle = '#ddd';
 
     // Top left corner + top line
-    ctx.moveTo(this.topLeftX, this.topLeftY + CURVE_RADIUS);
-    ctx.quadraticCurveTo(this.topLeftX, this.topLeftY, this.topLeftX + CURVE_RADIUS, this.topLeftY);
-    ctx.lineTo(bottomRightX - CURVE_RADIUS, this.topLeftY);
+    this.ctx.moveTo(this.topLeftX, this.topLeftY + CURVE_RADIUS);
+    this.ctx.quadraticCurveTo(this.topLeftX, this.topLeftY, this.topLeftX + CURVE_RADIUS, this.topLeftY);
+    this.ctx.lineTo(bottomRightX - CURVE_RADIUS, this.topLeftY);
 
     // Top right corner + right line
-    ctx.quadraticCurveTo(bottomRightX, this.topLeftY, bottomRightX, this.topLeftY + CURVE_RADIUS);
-    ctx.lineTo(bottomRightX, bottomRightY - CURVE_RADIUS);
+    this.ctx.quadraticCurveTo(bottomRightX, this.topLeftY, bottomRightX, this.topLeftY + CURVE_RADIUS);
+    this.ctx.lineTo(bottomRightX, bottomRightY - CURVE_RADIUS);
 
     // Bottom right corner + bottom line
-    ctx.quadraticCurveTo(bottomRightX, bottomRightY, bottomRightX - CURVE_RADIUS, bottomRightY);
-    ctx.lineTo(this.topLeftX + CURVE_RADIUS, bottomRightY);
+    this.ctx.quadraticCurveTo(bottomRightX, bottomRightY, bottomRightX - CURVE_RADIUS, bottomRightY);
+    this.ctx.lineTo(this.topLeftX + CURVE_RADIUS, bottomRightY);
 
     // Bottom left corner + left line
-    ctx.quadraticCurveTo(this.topLeftX, bottomRightY, this.topLeftX, bottomRightY - CURVE_RADIUS);
-    ctx.lineTo(this.topLeftX, this.topLeftY + CURVE_RADIUS);
+    this.ctx.quadraticCurveTo(this.topLeftX, bottomRightY, this.topLeftX, bottomRightY - CURVE_RADIUS);
+    this.ctx.lineTo(this.topLeftX, this.topLeftY + CURVE_RADIUS);
 
-    ctx.stroke();
-    ctx.fill();
+    this.ctx.stroke();
+    this.ctx.fill();
 
     var _drawIOs = (positions, ioX, initY) => {
       const numIO = Object.keys(positions).length;
@@ -57,20 +58,20 @@ class Node {
       for (const positionLabel in positions) {
         var ioY = initY + (ioNumber * ioYSpacing);
 
-        ctx.beginPath();
-        ctx.arc(ioX, ioY, IO_RADIUS, 0, Math.PI * 2);
-        ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(ioX, ioY, IO_RADIUS, 0, Math.PI * 2);
+        this.ctx.stroke();
 
-        ctx.fillStyle = '#fff';
-        ctx.fill();
+        this.ctx.fillStyle = '#fff';
+        this.ctx.fill();
 
         // Save position
         positions[positionLabel] = { x: ioX, y: ioY };
 
         if (numIO > 1) {
-          ctx.fillStyle = '#444';
-          ctx.font = '0.1em sans-serif';
-          ctx.fillText(positionLabel, ioX - 3, ioY + 3);
+          this.ctx.fillStyle = '#444';
+          this.ctx.font = '0.1em sans-serif';
+          this.ctx.fillText(positionLabel, ioX - 3, ioY + 3);
         }
 
         ++ioNumber;
@@ -91,7 +92,7 @@ class Node {
     }
     var output = this.outputs[outputNumber];
     var input  = targetNode.inputs[Node.INPUT_DEFAULT];
-    drawEdge(output.x + IO_RADIUS, output.y, input.x - IO_RADIUS, input.y)
+    _drawEdge(this.ctx, output.x + IO_RADIUS, output.y, input.x - IO_RADIUS, input.y)
   }
 }
 
@@ -99,16 +100,48 @@ Node.INPUT_DEFAULT  = 1;
 Node.OUTPUT_DEFAULT = 1;
 
 class PluginNode extends Node {
-  constructor(topLeftX, topLeftY, width, height) {
-    super(topLeftX, topLeftY, width, height, [ Node.INPUT_DEFAULT ], [ Node.OUTPUT_DEFAULT ]);
+  constructor(ctx, topLeftX, topLeftY, width, height) {
+    super(ctx, topLeftX, topLeftY, width, height, [ Node.INPUT_DEFAULT ], [ Node.OUTPUT_DEFAULT ]);
   }
 }
 
 class IfNode extends Node {
-  constructor(topLeftX, topLeftY, width, height) {
-    super(topLeftX, topLeftY, width, height, [ Node.INPUT_DEFAULT ], [ IfNode.OUTPUT_TRUE, IfNode.OUTPUT_FALSE ]);
+  constructor(ctx, topLeftX, topLeftY, width, height) {
+    super(ctx, topLeftX, topLeftY, width, height, [ Node.INPUT_DEFAULT ], [ IfNode.OUTPUT_TRUE, IfNode.OUTPUT_FALSE ]);
   }
 }
 
 IfNode.OUTPUT_TRUE = 'T';
 IfNode.OUTPUT_FALSE = 'F';
+
+function _drawEdge(ctx, fromX, fromY, toX, toY) {
+
+  // Make space for arrowhead
+  toX = toX - 20;
+
+  ctx.beginPath();
+  ctx.moveTo(fromX, fromY);
+  ctx.lineWidth = 1.5;
+  ctx.strokeStyle = '#555';
+  ctx.fillStyle = ctx.strokeStyle;
+
+  // Determine control points
+  control1X = (fromX + toX) * 0.6;
+  control1Y = fromY; // (fromY + toY) / 2;
+
+  control2X = (fromX + toX) * 0.4;
+  control2Y = toY; // (fromY + toY) / 2;
+
+  // Arrow curve
+  ctx.bezierCurveTo(control1X, control1Y, control2X, control2Y, toX, toY);
+  ctx.stroke();
+
+  // Arrow head
+  ctx.beginPath();
+  ctx.moveTo(toX, toY);
+  ctx.lineTo(toX, toY - 10); // Up
+  ctx.lineTo(toX + 20, toY); // Right + down
+  ctx.lineTo(toX, toY + 10); // Left + down
+  ctx.lineTo(toX, toY); // Up
+  ctx.fill();
+}
